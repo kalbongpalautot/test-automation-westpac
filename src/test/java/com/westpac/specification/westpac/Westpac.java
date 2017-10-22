787a598ff5209b214078b221f82a7006be684e27
MarcG <MarcG@192.168.1.9>
2017-10-22 22:28:52 +1300

    Deleted duplicated files

--- src/test/java/com/westpac/specification/westpac/Westpac.java
+++
@@ -1,7 +0,0 @@
-package com.westpac.specification.westpac;
-
-import com.westpac.specification.ConcordionIndex;
-
-public class Westpac extends ConcordionIndex {
-
-}
--- src/test/resources/com/westpac/specification/westpac/Westpac.md
+++
@@ -1,4 +0,0 @@
-# Westpac
-* [Test User Story One](TestUserStoryOne.html "c:run")  
-* [Test User Story Two](TestUserStoryTwo.html "c:run")  
-  
